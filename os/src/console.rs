8b1955dfa003451b9f2efcafe6d642fa76025435
Sirui Mu <msrlancern@gmail.com>
2021-09-01 22:15:04 +0800

    Finish chapter 1

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,10 +1,13 @@
+use core::fmt::Write;
+
+use log::{Level, LevelFilter, Log, Metadata, Record};
+
 use crate::sbi::console_putchar;
-use core::fmt::{self, Write};
 
 struct Stdout;
 
 impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
         for c in s.chars() {
             console_putchar(c as usize);
         }
@@ -12,7 +15,7 @@ impl Write for Stdout {
     }
 }
 
-pub fn print(args: fmt::Arguments) {
+pub fn print(args: core::fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
@@ -28,4 +31,43 @@ macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
+}
+
+struct Logger;
+
+impl Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        match record.level() {
+            Level::Error => {
+                print!("\x1b[31m[ERR] ");
+            },
+            Level::Warn => {
+                print!("\x1b[93m[WRN] ");
+            },
+            Level::Info => {
+                print!("\x1b[34m[INF] ");
+            },
+            Level::Debug => {
+                print!("\x1b[32m[DBG] ");
+            },
+            Level::Trace => {
+                print!("\x1b[90m[TRC] ");
+            },
+        };
+        print(*record.args());
+        println!("\x1b[0m");
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER_SINGLETON: Logger = Logger;
+
+pub fn init_log() {
+    log::set_logger(&LOGGER_SINGLETON);
+    log::set_max_level(LevelFilter::Trace);
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -4,6 +4,8 @@
 #![feature(llvm_asm)]
 #![feature(panic_info_message)]
 
+extern crate log;
+
 #[macro_use]
 mod console;
 mod lang_items;
@@ -34,14 +36,22 @@ pub fn rust_main() -> ! {
         fn boot_stack_top();
     }
     clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
+
+    console::init_log();
+
+    log::error!("Hello, world!");
+    log::warn!("Hello, world!");
+    log::info!("Hello, world!");
+    log::debug!("Hello, world!");
+    log::trace!("Hello, world!");
+
+    log::debug!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    log::debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    log::debug!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    log::debug!(
         "boot_stack [{:#x}, {:#x})",
         boot_stack as usize, boot_stack_top as usize
     );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    log::debug!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     panic!("Shutdown machine!");
 }
